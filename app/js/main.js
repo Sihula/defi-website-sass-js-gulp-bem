aa0d53128ab0821e7836180f0efe80690f6d24d4
Sihula <serhiisihula@gmail.com>
2021-12-26 17:31:38 +0100

    start

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+  $('.cards__slider').slick({
+    centerMode: true,
+    centerPadding: '360px',
+    slidesToShow: 3,
+    arrows: false,
+    dots: false,
+    autoplay: true
+  });
+
+});
+// const hamburger = document.querySelector('.burger');
+// const navLink = document.querySelector('.menu');
+
+// hamburger.addEventListener('click', () => {
+//   navLink.classList.toggle('active');
+// });
